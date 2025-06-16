682cf12826fd305d88684a9d6480e391c772b3d8
archis1405 <archismandas1405@gmail.com>
2025-06-16 15:48:27 +0530

    Added the previous and next buttons with functionality

--- src/Client/Components/HomeSectionSlider/HomeSectionSlider.jsx
+++ src/Client/Components/HomeSectionSlider/HomeSectionSlider.jsx
@@ -1,42 +1,80 @@
-import React from "react";
-import AliceCarousel from 'react-alice-carousel';
-import 'react-alice-carousel/lib/alice-carousel.css';
-import HomeProductCard from '../SectionCard/HomeProductCard.jsx';
-import KeyboardArrowLeftIcon from '@mui/icons-material/KeyboardArrowLeft';
+import React, { useState } from "react";
+import AliceCarousel from "react-alice-carousel";
+import "react-alice-carousel/lib/alice-carousel.css";
+import HomeProductCard from "../SectionCard/HomeProductCard.jsx";
+import KeyboardArrowLeftIcon from "@mui/icons-material/KeyboardArrowLeft";
 import { Button } from "@mui/material";
 
-
 const HomeSectionSlider = () => {
-    const responsive = {
-        0 : { items: 1 },
-        720 : { items: 3 },
-        1024 : { items: 5 },
-    };
+  const [activeIndex, setactiveIndex] = useState(0);
 
-    const items = [1,1,1,1,1,1,1,1,1,1].map((item)=><HomeProductCard /> )
-    return(
-        <div >
-            <div className="relative p-5">
-                <AliceCarousel
-                        items={items}
-                        disableButtonsControls
-                        infinite
-                        responsive={responsive}
-                        disableDotsControls
-                    />
-                    <Button variant="contained" className="z-50 bg-white" sx={{
-                        position: 'absolute' , top:'8rem' , right: '0rem' , transform:'translateX(50%) rotate(90deg)', bgcolor: 'white'}} aria-label = 'next' > 
-                        <KeyboardArrowLeftIcon sx={{transform: 'rotate(90deg)' , color: 'black'}}/>
-                    </Button>
+  const responsive = {
+    0: { items: 1 },
+    720: { items: 3 },
+    1024: { items: 5 },
+  };
 
-                    <Button variant="contained" className="z-50 bg-white" sx={{
-                        position: 'absolute' , top:'8rem' , left: '0rem' , transform:'translateX(-50%) rotate(-90deg)', bgcolor: 'white'}} aria-label = 'next' > 
-                        <KeyboardArrowLeftIcon sx={{transform: 'rotate(90deg)' , color: 'black'}}/>
-                    </Button>
-            </div>
-            
-        </div>
-    )
-}
+  const slidePreview = () => setactiveIndex(activeIndex - 1);
+  const slideNext = () => setactiveIndex(activeIndex + 1);
+
+  const syncActiveIndex = ({ item }) => setactiveIndex(item);
+
+  const items = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1].map((item) => (
+    <HomeProductCard />
+  ));
+  return (
+    <div>
+      <div className="relative p-5">
+        <AliceCarousel
+          items={items}
+          disableButtonsControls
+          infinite
+          responsive={responsive}
+          disableDotsControls
+          onSlideChanged={syncActiveIndex}
+          activeIndex={activeIndex}
+        />
+        {activeIndex !== items.length - 5 && (
+          <Button
+            variant="contained"
+            className="z-50 bg-white"
+            onClick={slideNext}
+            sx={{
+              position: "absolute",
+              top: "8rem",
+              right: "0rem",
+              transform: "translateX(50%) rotate(90deg)",
+              bgcolor: "white",
+            }}
+            aria-label="next"
+          >
+            <KeyboardArrowLeftIcon
+              sx={{ transform: "rotate(90deg)", color: "black" }}
+            />
+          </Button>
+        )}
+
+        {activeIndex !== items.length + 5 && (
+          <Button
+            variant="contained"
+            className="z-50 bg-white"
+            onClick={slidePreview}
+            sx={{
+              position: "absolute",
+              top: "8rem",
+              left: "0rem",
+              transform: "translateX(-50%) rotate(-90deg)",
+              bgcolor: "white",
+            }}
+          aria-label="next"
+        >
+          <KeyboardArrowLeftIcon
+            sx={{ transform: "rotate(90deg)", color: "black" }}
+          />
+        </Button>)}
+      </div>
+    </div>
+  );
+};
 
 export default HomeSectionSlider;
