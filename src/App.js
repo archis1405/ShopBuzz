748118674d20373156be36b2f9eb64c2915985e9
archis1405 <archismandas1405@gmail.com>
2025-06-17 23:07:43 +0530

    Added the product page

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import './App.css';
 import NavigationBar from './Client/Components/NavigationBar/navigationBar';
 import HomePage from './Client/Pages/Home/homePage';
 import Footer from './Client/Components/Footer/Footer';
+import Product from './Client/Components/Product/Product';
 
 function App() {
   return (
     <div className="App">
      <NavigationBar />
        <div>
-        <HomePage />
+        {/* <HomePage /> */}
+        <Product />
        </div>
          <Footer />
     </div>
